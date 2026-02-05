4c467e74fd313c2d4e8e26ee05ce2a4450adf077
melophe <sunlavins@gmail.com>
2026-02-05 21:20:11 +0900

    Add save and load functions for task persistence

--- main.go
+++ main.go
@@ -3,7 +3,37 @@ package main
 import (
 	"fmt"
 	"os"
+	"encoding/json"
 )
+
+type Task struct {
+	ID	  int
+	Name string
+	Done  bool
+}
+
+const taskFile = "tasks.json"
+
+// Save tasks to JSON file
+func saveTasks(tasks []Task) error {
+	data, err := json.MarshalIndent(tasks, "", " ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(taskFile, data, 0644)
+}
+
+// Load tasks from JSON file
+func loadTasks() ([]Task, error) {
+	data, err := os.ReadFile(taskFile)
+	if err != nil {
+		return []Task{}, nil
+	}
+
+	var tasks []Task
+	err = json.Unmarshal(data, &tasks)
+	return tasks, err
+}
 
 func main() {
 	// Check args: need at least a command
